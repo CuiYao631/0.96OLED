276bf221bf03faadb15f6a6d94269738f748bc4e
CuiYao631 <631836754@qq.com>
2021-07-15 17:50:04 +0800

    初步测试正常

---
+++ imagedata.h
@@ -0,0 +1,17 @@
+/*
+ * @Author: CuiYao 
+ * @Date: 2021-07-15 15:37:55 
+ * @Last Modified by: CuiYao
+ * @Last Modified time: 2021-07-15 15:39:29
+ */
+extern const unsigned char kaiji_0[];
+extern const unsigned char kaiji_10[];
+extern const unsigned char kaiji_20[];
+extern const unsigned char kaiji_30[];
+extern const unsigned char kaiji_40[];
+extern const unsigned char kaiji_50[];
+extern const unsigned char kaiji_60[];
+extern const unsigned char kaiji_70[];
+extern const unsigned char kaiji_80[];
+extern const unsigned char kaiji_90[];
+extern const unsigned char kaiji_100[];
